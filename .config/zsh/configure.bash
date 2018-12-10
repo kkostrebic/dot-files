2fdb979a2a6e1fe3e04889ec656d10301923275a
Helmut Schmidt <>
2018-12-10 13:57:38 +0100

    zsh configuration script to follow standard from vim one including backup and linking configuration files

--- .config/zsh/configure.bash
+++ .config/zsh/configure.bash
@@ -1,11 +1,20 @@
 #! /bin/bash
 
-TARGET=~/.zshrc
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+CONFIG_DIR=$HOME/.config/zsh
 
-# TODO: - copy (make original backup) instead of removing
-rm -f $TARGET
-ln -s $DIR/config $TARGET
+# TODO: - think of incremental backup...
+mv -u $CONFIG_DIR/config $CONFIG_DIR/config.old 2>/dev/null
+mv -u $HOME/.zshrc $HOME/.zshrc.old 2>/dev/null
+
+# create configuration directory if it's not there
+if [ ! -e $CONFIG_DIR ]; then
+  mkdir -p $CONFIG_DIR
+fi
+
+# make links to source configuration file
+ln -s $SOURCE_DIR/config $CONFIG_DIR/config
+ln -s $CONFIG_DIR/config $HOME/.zshrc
 
 # TODO: This should go to different file, but for now it'll be here
 # cat >>~/.zprofile <<EOL
