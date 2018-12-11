1d26f367e49df3282ac43546e449e9481503a352
Helmut Schmidt <>
2018-12-11 02:01:57 +0100

    dunst configuration script to follow 'standard' of other configuration scripts

--- .config/dunst/configure.bash
+++ .config/dunst/configure.bash
@@ -1,19 +1,32 @@
 #! /bin/bash
 
-I3V=~/.i3vars
-VAR=DUNST_CONFIG_DIR
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+CONFIG_DIR=$HOME/.config/dunst
+DUNST_FILE=/etc/xdg/dunst/dunstrc
 
-# TODO: - copy (make original backup) instead of removing
-#       - avoid using sudo 
-#
-# Remove old configuration 
-sudo rm -f /etc/xdg/dunst/dunstrc
+# TODO: - think of incremental backup...
+mv -u $CONFIG_DIR/config $CONFIG_DIR/config.old 2>/dev/null
+mv -u $CONFIG_DIR/signals $CONFIG_DIR/signals.old 2>/dev/null
+# TODO: - avoid using sudo
+sudo mv -u $DUNST_FILE $DUNST_FILE.old 2>/dev/null
 
-# Make to desired config file
-sudo ln -s $DIR/config /etc/xdg/dunst/dunstrc
+# create configuration directory if it's not there
+if [ ! -e $CONFIG_DIR ]; then
+  mkdir -p $CONFIG_DIR
+fi
 
-# If custom profile file does not exist or does not contain DUNST_CONFIG_DIR
-if [ ! -e $I3V ] || ! grep -q -F "$VAR=$DIR" $I3V; then
-  echo $VAR=$DIR >> $I3V
+# make links to source configuration file
+ln -s $SOURCE_DIR/config $CONFIG_DIR/config
+ln -s $SOURCE_DIR/signals $CONFIG_DIR/signals
+# TODO: - avoid using sudo
+sudo ln -s $CONFIG_DIR/config $DUNST_FILE
+
+# set environment variables for i3blocks
+ENV_FILE=~/.i3env
+VAR_NAME=DUNST_CONFIG_DIR
+
+# https://shreevatsa.wordpress.com/2008/03/30/zshbash-startup-files-loading-order-bashrc-zshrc-etc/
+# If custom profile file does not exist or does not contain I3BLOCKS_CONFIG_DIR
+if [ ! -e $ENV_FILE ] || ! grep -q -F "$VAR_NAME=$CONFIG_DIR" $ENV_FILE; then
+  echo $VAR_NAME=$CONFIG_DIR >> $ENV_FILE
 fi
