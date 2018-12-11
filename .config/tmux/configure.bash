977e6206fe6771a56d5f9a3763c8fff0dcde1474
Helmut Schmidt <>
2018-12-11 01:58:03 +0100

    added configuration script for tmux

--- .config/tmux/configure.bash
+++ .config/tmux/configure.bash
@@ -1,7 +1,17 @@
 #! /bin/bash
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+CONFIG_DIR=$HOME/.config/tmux
 
-# TODO: - copy (make original backup) instead of removing
-rm -f ~/.tmux.conf
-ln -s $DIR/config ~/.tmux.conf
+# TODO: - think of incremental backup...
+mv -u $CONFIG_DIR/config $CONFIG_DIR/config.old 2>/dev/null
+mv -u $HOME/.tmux.conf $HOME/.tmux.conf.old 2>/dev/null
+
+# create configuration directory if it's not there
+if [ ! -e $CONFIG_DIR ]; then
+  mkdir -p $CONFIG_DIR
+fi
+
+# make links to source configuration file
+ln -s $SOURCE_DIR/config $CONFIG_DIR/config
+ln -s $CONFIG_DIR/config $HOME/.tmux.conf
