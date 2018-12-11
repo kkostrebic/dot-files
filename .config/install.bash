5fcc7c496d6121aa896deac5109e0f991000c22f
Helmut Schmidt <>
2018-12-11 02:04:55 +0100

    included sub-configuration scripts that are finished and use absolete path to invoke those

--- .config/install.bash
+++ .config/install.bash
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-./vim/configure.bash
-./zsh/configure.bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+
+$DIR/zsh/configure.bash
+$DIR/vim/configure.bash
+$DIR/tmux/configure.bash
+$DIR/i3/configure.bash
+$DIR/i3blocks/configure.bash
+$DIR/rofi/configure.bash
+$DIR/dunst/configure.bash
