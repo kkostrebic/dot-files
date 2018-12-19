d0c8a19501c19685d3c693de822daaa453887fc8
Helmut Schmidt <>
2018-12-19 21:39:39 +0100

    focus thunderbird mail client on mail indicator click

--- .config/i3blocks/commands/mail_notification_handler.bash
+++ .config/i3blocks/commands/mail_notification_handler.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ $BLOCK_BUTTON -eq 1 ]]; then
+  i3-msg "[class=Thunderbird] focus"
   printf '{"full_text":"", "color":"#555555"}\n'
 else
   printf '{"full_text":"", "color":"#00FFFF"}\n'
