c370f9899c0ca6170134c2565045315c9c0a96fb
Helmut Schmidt <>
2018-12-25 11:52:38 +0000

    moved xresources to dedicated directory

---
+++ .config/xterm/configure.bash
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+CONFIG_DIR=$HOME/.config/xterm
+
+# TODO: - think of incremental backup...
+mv -u $CONFIG_DIR/config $CONFIG_DIR/config.old 2>/dev/null
+mv -u $HOME/.Xdefaults $HOME/.Xdefaults.old 2>/dev/null
+
+# create configuration directory if it's not there
+if [ ! -e $CONFIG_DIR ]; then
+  mkdir -p $CONFIG_DIR
+fi
+
+# make links to source configuration file
+ln -s $SOURCE_DIR/config $CONFIG_DIR/config
+ln -s $CONFIG_DIR/config $HOME/.Xdefaults
