8f141a916a5461c73cd6761c7ced5e9ad15877d0
Helmut Schmidt <>
2018-12-26 01:15:46 +0000

    added caps lock indicator for i3blocks

---
+++ .config/i3blocks/commands/key_notification_handler.bash
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+CAPS=$(xset -q | grep Caps | awk '{ print $4 }')
+
+if [[ $CAPS == 'on' ]]; then
+  printf '{ "full_text":"CAPS", "color":"#00FF00" }\n'
+else
+  printf '{ "full_text":"CAPS", "color":"#555555" }\n'
+fi
