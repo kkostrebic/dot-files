02a3994288fae6681affe3f0f29588a587fab2b4
Helmut Schmidt <>
2019-01-02 16:10:32 +0100

    i3blocks needs to be installed building it from sources

---
+++ .config/i3blocks/install.bash
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+git clone https://github.com/vivien/i3blocks
+cd i3blocks
+./autogen.sh
+./configure --prefix=/usr/local/stow/i3blocks
+make
+make install
+
+rm -rf i3blocks
+cd /usr/local/stow 
+stow i3blocks
