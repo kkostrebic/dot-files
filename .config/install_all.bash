cc06ff5868802036268aa0e23ba8d88d02360a3f
Helmut Schmidt <>
2019-01-02 16:16:00 +0100

    added couple of more packages to installation script

--- .config/install_all.bash
+++ .config/install_all.bash
@@ -2,9 +2,22 @@
 
 apt update
 
+# i3lock - screen locker
+# xdotool - fake keyboard/mouse input, window management, etc.
+# mupdf - micro pdf reader
+# wkhtmltopdf - html to pdf
+# feh - image viewer
+# pavucontrol - pulse audio volume control
+# wicd - network gui manager
+# blueman - bluetooth gui manager
+# mc - midnight commander - tui file manager 
+# thunar - gui file manager from xfce
+# stow - tool for managing the installation of multiple software packages in the same run-time directory tree
 apt install -y build-essential \
+               module-assistant \
                net-tools \
                xinit \
+               iw \
                wireless-tools \
                bluetooth \
                bluez \
@@ -14,7 +27,37 @@ apt install -y build-essential \
                subversion \
                git \
                curl \
-               wget
+               wget \
+               dh-autoreconf \
+               default-jdk \
+               i3 \
+               xterm \
+               dunst \
+               rofi \
+               tmux \                 
+               vim-gtk3 \
+               i3lock \
+               stow \
+               x11-xserver-utils \
+               pavucontrol \
+               wicd \
+               blueman \
+               xdotool \
+               feh \
+               mupdf \
+               xfonts-base \
+               fonts-dejavu-core \
+               fonts-font-awesome \
+               autoconf \
+               notify-osd \
+               thunderbird \
+               libreoffice \
+               gimp \
+               vlc \
+               wkhtmltopdf \
+               mc \
+               thunar 
+
 
 # ripgrep (NOTE: from 18.10 available through standard packaging)
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
@@ -24,52 +67,16 @@ sudo dpkg -i ripgrep_0.10.0_amd64.deb
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 
-apt install -y xterm \
-               i3blocks \
-               dunst \
-               rofi \
-               tmux \
-               vim-gtk3
-
+./i3blocks/install.bash
 ./zsh/install.bash
 
-# i3lock - screen locker
-# xdotool - fake keyboard/mouse input, window management, etc.
-# mupdf - micro pdf reader
-# wkhtmltopdf - html to pdf
-# feh - image viewer
-# pavucontrol - pulse audio volume control
-# mc - midnight commander - tui file manager 
-# thunar - gui file manager from xfce
-# stow - tool for managing the installation of multiple software packages in the same run-time directory tree
-apt install -y i3lock \
-               stow \
-               x11-xserver-utils \
-               network-manager \
-               network-manager-gnome \
-               pavucontrol \
-               xdotool \
-               feh \
-               mupdf \
-               xfonts-base \
-               fonts-dejavu-core \
-               fonts-font-awesome \
-               autoconf \
-               notify-osd \
-               firefox \
-               thunderbird \
-               libreoffice \
-               virtualbox \
-               gimp \
-               vlc \
-               wkhtmltopdf \
-               mc \
-               thunar \
-
+m-a prepare
 
 # slack
 # wireshark
 # clipit - clipboard manager
+# network-manager \
+# network-manager-gnome \
 
 # Online tools:
 # draw.io
