49af61b40aef356c2e96143f0ca33f37ae19d061
Helmut Schmidt <>
2019-01-02 16:11:28 +0100

    link xterm configuration to both xdefaults and xresources

--- .config/xterm/configure.bash
+++ .config/xterm/configure.bash
@@ -6,6 +6,7 @@ CONFIG_DIR=$HOME/.config/xterm
 # TODO: - think of incremental backup...
 mv -u $CONFIG_DIR/config $CONFIG_DIR/config.old 2>/dev/null
 mv -u $HOME/.Xdefaults $HOME/.Xdefaults.old 2>/dev/null
+mv -u $HOME/.Xresources $HOME/.Xresources.old 2>/dev/null
 
 # create configuration directory if it's not there
 if [ ! -e $CONFIG_DIR ]; then
@@ -15,3 +16,4 @@ fi
 # make links to source configuration file
 ln -s $SOURCE_DIR/config $CONFIG_DIR/config
 ln -s $CONFIG_DIR/config $HOME/.Xdefaults
+ln -s $CONFIG_DIR/config $HOME/.Xresources
