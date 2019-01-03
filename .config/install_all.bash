0d4bdc5e0059ca2d074e3594dd515d334c416d5e
Helmut Schmidt <>
2019-01-03 11:14:07 +0100

    install and configure nvm

--- .config/install_all.bash
+++ .config/install_all.bash
@@ -69,6 +69,9 @@ sudo dpkg -i ripgrep_0.10.0_amd64.deb
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 
+# NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
 ./i3blocks/install.bash
 ./zsh/install.bash
 
