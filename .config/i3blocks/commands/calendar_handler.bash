decce6e6ec858f6a47bba0513f99a3bb00a3c094
Helmut Schmidt <>
2019-01-11 01:42:44 +0000

    i3blocks rearrangements; handle silent category notifications; logic for 'health' indicators

--- .config/i3blocks/commands/calendar_handler.bash
+++ .config/i3blocks/commands/calendar_handler.bash
@@ -4,5 +4,6 @@ if [[ $BLOCK_BUTTON -eq 1 ]]; then
   orage 1>/dev/null 2>&1 & 
 fi
 
-date '+%Y-%m-%d %H:%M:%S'
+# date '+%Y-%m-%d %H:%M:%S'
+date '+%H:%M'
 exit 0
---
+++ .config/i3blocks/commands/exercise_handler.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $_enabled -eq 0 ]]; then
+  if [[ $BLOCK_BUTTON -eq 3 ]]; then
+    printf '{"full_text":"", "color":"#555555", "_enabled":1, "_skip_initial":0}\n'
+  else
+    printf '{"full_text":"", "color":"#555555", "_enabled":0, "_skip_initial":0}\n'
+  fi
+else
+  if [[ $BLOCK_BUTTON -eq 1 ]]; then
+    printf '{"full_text":"", "color":"#555555", "_enabled":1, "_skip_initial":0}\n'
+  elif [[ $BLOCK_BUTTON -eq 3 ]]; then
+    printf '{"full_text":"", "color":"#555555", "_enabled":0, "_skip_initial":0}\n'
+  elif [[ $_skip_initial -eq 1 ]]; then
+    printf '{"full_text":"", "color":"#555555", "_enabled":1, "_skip_initial":0}\n'
+  else
+    notify-send -t 5000 -c "silent" "Take a walk; Locking in 5 seconds..."
+    (sleep 5 && i3lock -c 000000) &
+    printf '{"full_text":"", "color":"#00FFFF", "_enabled":1, "_skip_initial":0}\n'
+  fi
+fi
+
+exit 0
+
