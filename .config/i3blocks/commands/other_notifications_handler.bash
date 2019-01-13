4b822b508c51258c72352dcf7cd2a64b426824f8
Helmut Schmidt <>
2019-01-13 00:27:19 +0000

    changed icon for notifications; modified notification handler to follow few of best practices in writing bash scripts; enabled/disabled notification daemon functionality; do not show caps lock indicator if it's not on

--- .config/i3blocks/commands/other_notifications_handler.bash
+++ .config/i3blocks/commands/other_notifications_handler.bash
@@ -1,12 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ $full_text == "" && $BLOCK_BUTTON -eq 1 ]]; then
-  printf '{ "full_text":"", "color":"#555555" }\n'
-  if [[ $color == "#00FFFF" ]]; then
-    for run in {1..20}; do xdotool key Ctrl+grave; done
-  fi
-  # bash -c 'sleep 20; killall dunst; dunst &' &
-else
-  printf '{ "full_text":"", "color":"#00FFFF" }\n'
+# font-awesome 4 unicodes:
+# f0f3 - bell
+# f1f6 - bell-slash
+bell=
+bell_slash=
+no=0
+yes=1
+gray="#555555"
+turquoise="#00FFFF"
+left_click=1
+right_click=3
+
+function print_json {
+  message='{ "full_text":"%s", "color":"%s", "_new_notifications":%d, "_enabled":%d}\n'
+  printf "$message" "$1" "$2" "$3" "$4"
+}
+
+function start_notification_daemon {
+  exec -c dunst
+}
+
+function stop_notification_daemon {
+  killall dunst
+}
+
+function show_new_notifications {
+  start=1
+  while [[ $start -le $_new_notifications ]]; do 
+    xdotool key Ctrl+grave
+    ((start = start + 1)) 
+  done
+}
+
+if [[ $BLOCK_BUTTON -eq $left_click ]] && [[ $_new_notifications -gt 0 ]]; then
+  show_new_notifications
+  print_json "$bell" "$gray" 0 $yes
+elif [[ $BLOCK_BUTTON -eq $right_click ]]; then
+  case $_enabled in
+    $yes)
+      stop_notification_daemon
+      print_json "$bell_slash" "$gray" 0 $no
+      ;;
+    $no)
+      print_json "$bell" "$gray" 0 $yes
+      start_notification_daemon
+      ;;
+  esac
+elif [[ $BLOCK_BUTTON -eq 0 ]]; then
+  ((_new_notifications = _new_notifications + 1))
+  print_json "$bell" "$turquoise" $_new_notifications $yes
 fi
+
 exit 0
