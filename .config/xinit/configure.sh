cd0f16e6311bc206c7b9fb1a27ed60a9858bb9f0
Helmut Schmidt <>
2019-02-20 21:53:46 +0000

    additional comments on xinit

--- .config/xinit/configure.sh
+++ .config/xinit/configure.sh
@@ -4,6 +4,13 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 
 # TODO: - think of incremental backup...
 mv -u $HOME/.xinitrc $HOME/.xinitrc.old 2>/dev/null
+# mv -u $HOME/.zprofile $HOME/.zprofile.old 2>/dev/null
 
 # make links to source configuration file
 ln -s $SOURCE_DIR/config $HOME/.xinitrc
+
+# cat >>~/.zprofile <<EOL
+# if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+#   exec startx
+# fi
+# EOL
