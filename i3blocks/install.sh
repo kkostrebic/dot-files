aa1cabcc4e48763446813aebe1648ceb626cbc00
Helmut Schmidt <>
2019-02-27 23:31:51 +0000

    set correct folder when building i3blocks from source

--- i3blocks/install.sh
+++ i3blocks/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+cd $DIR
+
 git clone https://github.com/vivien/i3blocks
 cd i3blocks
 ./autogen.sh
@@ -7,6 +10,7 @@ cd i3blocks
 make
 sudo make install
 
+cd $DIR
 rm -rf i3blocks
 cd /usr/local/stow
 sudo stow i3blocks
