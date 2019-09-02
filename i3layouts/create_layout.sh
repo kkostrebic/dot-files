0714ba4525faa0d02b11e4c419b0219b93fc5b37
Helmut Schmidt <>
2019-09-02 12:26:30 +0200

    don't use hardcoded path to layout in create_layout script

--- i3layouts/create_layout.sh
+++ i3layouts/create_layout.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 WORKSPACE="workspace $1"
-LAYOUT="/home/helmut/projects/dot-files/i3layouts/$2"
+FULL_PATH="$(cd "$(dirname "$0")"; pwd -P)"
+LAYOUT="$FULL_PATH/$2"
 APPS="exec xterm; exec xterm; exec xterm; exec gvim; exec gvim; exec gvim; exec google-chrome; exec google-chrome;"
 
 i3-msg "$WORKSPACE; append_layout $LAYOUT; $APPS"
