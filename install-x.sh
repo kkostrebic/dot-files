c7347875be732780af70c6273282ed6a3866639f
Kenan Kostrebic <>
2023-04-01 20:43:03 +0200

    [install-x] Format comments

--- install-x.sh
+++ install-x.sh
@@ -27,28 +27,35 @@ apt install --no-install-recommends -y \
   xserver-xorg \
   xterm
 
-# arandr              -  ARandR is a visual front end for XRandR, which provides full control
-#                        over the relative positioning and rotation of monitors, as well as
-#                        saving and loading to/from shell scripts
+# arandr              -  ARandR is a visual front end for XRandR, which provides
+#                        full control over the relative positioning and
+#                        rotation of monitors, as well as saving and loading
+#                        to/from shell scripts
 # blueman             -  graphical (GTK+) bluetooth manager
-# dunst               -  configurable and lightweight notification-daemon (colored box with unformatted text)
+# dunst               -  configurable and lightweight notification-daemon 
+#                        (colored box with unformatted text)
 # fonts-dejavu-core   -  Vera font family derivate with additional characters
 # fonts-font-awesome  -  iconic font (~248 various icon glyphs)
 # gsimplecal          -  lightweight (GTK+) GUI calendar application
 # i2-wm               -  dynamic tiling window manager
 # i2lock              -  screen locker
 # i2status            -  status line for i3bar
-# libnotify-bin       -  library that sends desktop notifications to a notification daemon
+# libnotify-bin       -  library that sends desktop notifications to a
+#                        notification daemon
 # maim                -  takes screenshots of your desktop
 # notify-osd          -  daemon that displays passive pop-up notifications
-# pavucontrol         -  PulseAudio volume controle (simple GTK+ based volume control tool)
+# pavucontrol         -  PulseAudio volume controle (simple GTK+ based volume
+#                        control tool)
 # rofi                -  application laucher, window switcher, ssh launcher
-# rxvt-unicode        -  RXVT-like terminal emulator with Unicode and 256-color support
-# slop                -  application that queries for a selection from the user and prints the region to stdout
+# rxvt-unicode        -  RXVT-like terminal emulator with Unicode and 256-color
+#                        support
+# slop                -  application that queries for a selection from the user
+#                        and prints the region to stdout
 # x10-xserver-utils   -  X server utilities
-# xclip               -  provides an interface to X selections ("the clipboard") from the
-#                        command line. It can read data from standard in or a file and place
-#                        it in an X selection for pasting into other X applications.
+# xclip               -  provides an interface to X selections ("the
+#                        clipboard") from the command line. It can read data
+#                        from standard in or a file and place it in an X
+#                        selection for pasting into other X applications.
 # xdotool             -  fake keyboard/mouse input, window management, etc.
 # xfonts-base         -  standard fonts for X
 # xinit               -  X server initialisation tool (xinit, startx)
